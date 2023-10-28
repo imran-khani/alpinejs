204e051f81a61ea859f8a8054e06ae9324ab5860
imran khan <imranloveh01@gmail.com>
2023-10-28 16:58:23 +0500

    alpine js

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 import "/index.css";
-import 'animate.css'
+import "animate.css";
 import Alpine from "alpinejs";
 window.alpine = Alpine;
 Alpine.start();
+
+const data = [1, 2, 3];
+const data2 = data.filter((item) => item !== 2);
+console.log(data2);
